faf023d4896ecf86e3008fc04fe8c8b26027b598
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2022-09-22 15:34:01 -0300

    feat(Requisito 1): Cria a classe Race, os atributos e métodos requeridos no readme

---
+++ src/Races/Race.ts
@@ -0,0 +1,39 @@
+// A classe Race deve ter os atributos privados: name e dexterity, ambos inicializados em seu construtor;
+export default abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  public get name(): string {
+    return this.name;
+  }
+
+  // public set name(value: string) {
+  //   this._name = value;
+  // }
+
+  public get dexterity(): number {
+    return this.dexterity;
+  }
+
+  // public set dexterity(value: number) {
+  //   this._dexterity = value;
+  // }
+
+  static createdRacesInstances(/* maxInstanceNumber: number */): number {
+    // let count = 0;
+    // if (count === maxInstanceNumber) {
+    //   throw new Error('Not implemented');
+    // }
+    throw new Error('Not implemented');
+
+    // count += count + 1;
+    // return count;
+  }
+
+  abstract get maxLifePoints(): number;
+}
